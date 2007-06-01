5b4d6fbb2d11e7a69df2cccc6599007eb1b758e2
joe.gregorio <joe.gregorio@2a8e73a3-2531-0410-a65f-257249c96f84>
2007-06-01 16:54:14 +0000

    Added pastebin sample

---
+++ samples/pastebin/models/bin.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Table, Column, Integer, String, VARCHAR
+import dbconfig
+
+table = Table('bin', dbconfig.metadata,
+        Column('id', Integer(), primary_key=True),
+        Column('code', VARCHAR()),
+        Column('language', String(50))
+        )
+
