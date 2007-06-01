a9060357513324147fd0d3c369b889d5a7760cf1
joe.gregorio <joe.gregorio@2a8e73a3-2531-0410-a65f-257249c96f84>
2007-06-01 21:04:30 +0000

    Added in the filename column, and also added a list of bins to the main page.

--- samples/pastebin/models/bin.py
+++ samples/pastebin/models/bin.py
@@ -4,6 +4,7 @@ import dbconfig
 table = Table('bin', dbconfig.metadata,
         Column('id', Integer(), primary_key=True),
         Column('code', VARCHAR()),
-        Column('language', String(50))
+        Column('language', String(50)),
+        Column('filename', String(50))
         )
 
